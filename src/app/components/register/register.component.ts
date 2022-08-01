0bc9a8597cd4dc467ecad96f25b5f4cdd81f2c19
tuepi <trandinhtue140398@gmail.com>
2022-08-02 06:22:17 +0700

    xong validate và hiển thị cảnh báo ở trang đăng ký

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -13,31 +13,54 @@ export class RegisterComponent implements OnInit {
 
 
   registerForm: FormGroup = new FormGroup({
-    username: new FormControl(),
-    fullName: new FormControl(),
-    phone: new FormControl(),
-    password: new FormControl(),
-    confirmPassword: new FormControl(),
+    username: new FormControl('', [Validators.required, Validators.maxLength(32)]),
+    fullName: new FormControl('', [ Validators.minLength(6), Validators.maxLength(32)]),
+    phone: new FormControl('', [Validators.required,Validators.pattern("(03|05|07|08|09)+([0-9]{8})")]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
   });
 
   constructor(private authenticationService: AuthenticationService,
               private router: Router
-  ) {
+  ) {}
+
+  get username(){
+    return this.registerForm.get('username')
   }
+
+  get fullName(){
+    return this.registerForm.get('fullName')
+  }
+  get password(){
+    return this.registerForm.get('password')
+  }
+
+  get confirmPassword(){
+    return this.registerForm.get('confirmPassword')
+  }
+
+  get phone(){
+    return this.registerForm.get('phone')
+  }
+
 
   ngOnInit(): void {
   }
 
   register() {
     const user = this.setNewUser();
-    this.authenticationService.register(user).subscribe((data) => {
-      console.log(data);
-      alert("Thành công")
-      this.registerForm.reset();
-      this.router.navigate(['/login']);
-    }, err => {
-      console.log(err);
-    });
+    if (user.password === user.confirmPassword) {
+      this.authenticationService.register(user).subscribe((data) => {
+        console.log(data);
+        alert("Thành công")
+        this.registerForm.reset();
+        this.router.navigate(['/login']);
+      }, err => {
+        console.log(err);
+      });
+    } else {
+      alert("Mật khẩu không trùng khớp");
+    }
   }
 
   private setNewUser() {
@@ -50,6 +73,8 @@ export class RegisterComponent implements OnInit {
     };
     return user;
   }
+
+
 
 
 
