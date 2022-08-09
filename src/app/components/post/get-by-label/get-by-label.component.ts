7d5d0d161e15c799cb75da24ccce0a4a7c45168a
kenproxx <kienhue98@gmail.com>
2022-08-09 17:41:05 +0700

    ve thoi

--- src/app/components/post/get-by-label/get-by-label.component.ts
+++ src/app/components/post/get-by-label/get-by-label.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PostLabelService} from "../../../services/post-label.service";
 
 @Component({
   selector: 'app-get-by-label',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./get-by-label.component.css']
 })
 export class GetByLabelComponent implements OnInit {
+  listPostByLabel : any = []
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private postLabelService: PostLabelService) {
   }
 
+  ngOnInit(): void {
+    this.getPostByLabel()
+  }
+
+  getPostByLabel() {
+    let labelId = localStorage.getItem('labelId')
+    this.listPostByLabel = []
+    this.postLabelService.getAllPostLabel(labelId).subscribe((data) => {
+      this.scanData(data)
+      console.log(this.listPostByLabel)
+
+      localStorage.removeItem('labelId')
+    } )
+  }
+
+  scanData(data: any[]) {
+    for (let i = 0; i < data.length; i++) {
+      this.listPostByLabel.push(data[i].post)
+    }
+  }
 }
--- src/app/components/post/top5-by-likes/top5-by-likes.component.ts
+++ src/app/components/post/top5-by-likes/top5-by-likes.component.ts
@@ -14,7 +14,6 @@ export class Top5ByLikesComponent implements OnInit {
 
   top5Posts : Post[] | any
   listLabel: any = []
-  listPostByLabel : any = []
 
   constructor(private postService : PostService,
               private labelService: LabelService,
@@ -27,7 +26,6 @@ export class Top5ByLikesComponent implements OnInit {
 
   top5ByLikes() {
     this.postService.top5ByLikes().subscribe(data => {
-        console.log(data)
         this.top5Posts = data;
       },
       error => {
@@ -41,11 +39,9 @@ export class Top5ByLikesComponent implements OnInit {
     })
   }
 
-  getPostByLabel(labelId: any) {
-    this.postLabelService.getAllPostLabel(labelId).subscribe((data) => {
-      this.listPostByLabel = data;
-    } )
-  }
+setLabel(labelId: any) {
+    localStorage.setItem('labelId', labelId)
+}
 
 
 
